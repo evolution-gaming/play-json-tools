5e2c9947f387b451458aecec26946b411ba5a853
Nikita Matveenko <nikitapecasa@gmail.com>
2018-02-22 10:48:32 +0200

    Add more tests for MapFormat

--- src/test/scala/com/evolutiongaming/util/MapFormatSpec.scala
+++ src/test/scala/com/evolutiongaming/util/MapFormatSpec.scala
@@ -1,15 +1,13 @@
 package com.evolutiongaming.util
 
 import com.evolutiongaming.util.JsonFormats._
+import com.evolutiongaming.util.MapFormatSpec._
 import org.scalatest.{Matchers, WordSpec}
-import play.api.libs.json.{JsSuccess, Json}
+import play.api.libs.json.{JsSuccess, Json, OFormat}
 
 class MapFormatSpec extends WordSpec with Matchers {
 
   "MapFormat" should {
-    
-    implicit val valueFormat = Json.format[Value]
-    implicit val keyFormat = Json.format[Key]
 
     val mapFormat = new MapFormat[Key, Value]("id")
 
@@ -68,6 +66,48 @@ class MapFormatSpec extends WordSpec with Matchers {
     }
   }
 
+  "MapFormat's key" should {
+
+    "not be named after a field of the value" in {
+      an[IllegalArgumentException] should be thrownBy {
+        new MapFormat[ComboKey, RichValue]("key")
+      }
+    }
+
+    "be present twice in result JSON if it's part of the value" in {
+      val mapFormat = new MapFormat[ComboKey, RichValue]("id")
+
+      val value1 = RichValue(ComboKey("k1_1", "k1_2"), Value(42))
+
+      val map: Map[ComboKey, RichValue] = Map((value1.key, value1))
+
+      val duplicatedKey = Json.obj("k1" -> "k1_1", "k2" -> "k1_2")
+      val json = Json.arr(
+        Json.obj(
+          "id" -> duplicatedKey,
+          "key" -> duplicatedKey,
+          "value" -> Json.obj("value" -> 42)
+        )
+      )
+
+      mapFormat.writes(map) shouldBe json
+      mapFormat.reads(json) shouldBe JsSuccess(map)
+    }
+
+  }
+
+}
+
+object MapFormatSpec {
   case class Key(value: String)
   case class Value(value: Int)
+
+  case class ComboKey(k1: String, k2: String)
+  case class RichValue(key: ComboKey, value: Value)
+
+  implicit val keyFormat: OFormat[Key] = Json.format[Key]
+  implicit val valueFormat: OFormat[Value] = Json.format[Value]
+
+  implicit val comboKeyFormat: OFormat[ComboKey] = Json.format[ComboKey]
+  implicit val richValueFormat: OFormat[RichValue] = Json.format[RichValue]
 }
