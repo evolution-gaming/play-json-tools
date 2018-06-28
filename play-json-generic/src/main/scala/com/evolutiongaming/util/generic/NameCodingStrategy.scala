f37d36cf3f051f234cb9fba0fbad526cbbe809a9
Maris Ruskulis <mruskulis@evolutiongaming.com>
2018-06-28 17:47:37 +0300

    improves namecoding

--- play-json-generic/src/main/scala/com/evolutiongaming/util/generic/NameCodingStrategy.scala
+++ play-json-generic/src/main/scala/com/evolutiongaming/util/generic/NameCodingStrategy.scala
@@ -1,31 +1,39 @@
 package com.evolutiongaming.util.generic
 
 trait NameCodingStrategy {
-  def encode(s: String): String
-  def decode(s: String): String
+  def encode(input: String): String
+  def matches(encoded: String, compareAgainst: String): Boolean
 }
 
 trait LowPriority {
   implicit val default: NameCodingStrategy = new NameCodingStrategy {
-    override def encode(s: String): String = s
-    override def decode(s: String): String = s
+    override def encode(input: String): String = input
+    override def matches(encoded: String, compareAgainst: String): Boolean = encoded == compareAgainst
   }
 }
 
 object NameCodingStrategy extends LowPriority
 
 object NameCodingStrategies {
-  implicit val kebabCase: NameCodingStrategy = new NameCodingStrategy {
-    override def encode(s: String): String =
-      s.foldLeft(List.empty[String]) {
+  private def lowerCaseSepCoding(sep: String): NameCodingStrategy = new NameCodingStrategy {
+
+    override def encode(input: String): String =
+      input.foldLeft(List.empty[String]) {
         case (ll, n) if n.isUpper =>
           n.toLower.toString :: ll
         case (h :: t, n)          =>
           h + n :: t
         case (nil, n)             =>
           n.toString :: nil
-      }.reverse.mkString("-")
+      }.reverse.mkString(sep)
 
-    override def decode(s: String): String = s.split("-").map(_.capitalize).mkString("")
+    override def matches(encoded: String, compareAgainst: String): Boolean =
+      encoded == encode(compareAgainst)
   }
+
+  implicit val kebabCase: NameCodingStrategy = lowerCaseSepCoding("-")
+
+  implicit val snakeCase: NameCodingStrategy = lowerCaseSepCoding("_")
+
+  implicit val noSepCase: NameCodingStrategy = lowerCaseSepCoding("")
 }
