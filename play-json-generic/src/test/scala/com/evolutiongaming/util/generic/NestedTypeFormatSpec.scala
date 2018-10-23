8e6754c9aff7bd625394d32b78b0a84ba2e609e8
Yuri Bonya <ybonya@evolutiongaming.com>
2018-10-23 13:05:12 +0300

    Style fixes

--- play-json-generic/src/test/scala/com/evolutiongaming/util/generic/NestedTypeFormatSpec.scala
+++ play-json-generic/src/test/scala/com/evolutiongaming/util/generic/NestedTypeFormatSpec.scala
@@ -5,24 +5,7 @@ import Message._
 
 class NestedTypeFormatSpec extends JsonFormatSpec {
 
-  "NestedTypeFormat" should {
-
-    "be able to read and write sealed trait hierarchy using full type name as a discriminator" in {
-
-      implicit val format = MessageFormat
-
-      for {
-        (o, json) <- Seq(
-          Noop -> Json.obj("type" -> "Noop"),
-          In.Update(42) -> Json.obj("type" -> "In.Update", "payload" -> 42),
-          Out.Updated("test") -> Json.obj("type" -> "Out.Updated", "v" -> "test"),
-          Out.Ack -> Json.obj("type" -> "Out.Ack")
-        )
-      } yield check[Message](o, json)
-    }
-  }
-
-  val MessageFormat = {
+  implicit val messageFormat = {
     implicit val noopFormat: OFormat[Noop.type] = new OFormat[Noop.type] {
       def writes(o: Noop.type): JsObject = Json.obj()
       def reads(json: JsValue): JsResult[Message.Noop.type] = JsSuccess(Message.Noop)
@@ -37,5 +20,19 @@ class NestedTypeFormatSpec extends JsonFormatSpec {
     implicit val updatedFormat: OFormat[Out.Updated] = Json.format[Out.Updated]
 
     NestedTypeFormat[Message]
+  }
+
+  "NestedTypeFormat" should {
+
+    "be able to read and write sealed trait hierarchy using full type name as a discriminator" in {
+      for {
+        (o, json) <- Seq(
+          Noop -> Json.obj("type" -> "Noop"),
+          In.Update(42) -> Json.obj("type" -> "In.Update", "payload" -> 42),
+          Out.Updated("test") -> Json.obj("type" -> "Out.Updated", "v" -> "test"),
+          Out.Ack -> Json.obj("type" -> "Out.Ack")
+        )
+      } yield check[Message](o, json)
+    }
   }
 }
