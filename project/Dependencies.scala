22b13871bc44a9946e1a1e31b7545941ebe01405
Yaroslav Klymko <t3hnar@gmail.com>
2019-05-12 01:16:59 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val shapeless = "com.chuusai"         %% "shapeless" % "2.3.3"
   val nel       = "com.evolutiongaming" %% "nel" % "1.3.3"
   val playJson  = "com.typesafe.play"   %% "play-json" % "2.7.3"
-  val scalaTest = "org.scalatest"       %% "scalatest" % "3.0.5"
+  val scalaTest = "org.scalatest"       %% "scalatest" % "3.0.7"
 
   def excludeLog4j(moduleID: ModuleID): ModuleID = moduleID.excludeAll(
     ExclusionRule("log4j", "log4j"),
