d924ee65e78f3a4e2c28853716bd1abc2be28e64
Vadim Bondarev <haghard84@gmail.com>
2020-01-09 17:41:34 +0100

    Add more tests

--- play-json-jsoniter/src/test/scala/com/evolutiongaming/jsonitertool/JsoniterSpec.scala
+++ play-json-jsoniter/src/test/scala/com/evolutiongaming/jsonitertool/JsoniterSpec.scala
@@ -8,6 +8,8 @@ import TestData._
 
 class JsoniterSpec extends AnyFunSuite with Matchers {
 
+  val maxDoubleStr = "179769313486231570814527423731704356798070567525844996598917476803157260780028538760589558632766878171540458953514382464234321326889464182768467546703537516986049910576551282076245490090389328944075868508455133942304583236903222948165808559332123348274797826204144723168738177180919299881250404026184124858368"
+
   test("Write using PlayJson -> Read using Jsoniter: Compare bytes") {
 
     val expected: DataLine = Json.fromJson[DataLine](Json.parse(TestData.jsonBody))
@@ -17,7 +19,6 @@ class JsoniterSpec extends AnyFunSuite with Matchers {
     val bts0 = PlayJsonJsoniter.serialize(jsValue)
     val bts1 = Json.toBytes(jsValue)
     java.util.Arrays.compare(bts0, bts1) shouldEqual 0
-
   }
 
   test("Write using PlayJson -> Read using Jsoniter: Compare objects") {
@@ -30,5 +31,30 @@ class JsoniterSpec extends AnyFunSuite with Matchers {
     val actual = Json.fromJson[DataLine](jsValue)
 
     JsSuccess(expected) shouldEqual actual
+  }
+
+  test("Can write/read large number by play-json") {
+    //when number size hits length 35, equality comparison doesn't work anymore
+    val largeNum = "9999999999999999999999999999999911"
+    val jsValue = play.api.libs.json.JsNumber(BigDecimal(largeNum))
+    println("")
+    val bytes = Json.toBytes(jsValue)
+    new String(bytes) shouldEqual largeNum
+    Json.parse(bytes) shouldEqual jsValue
+  }
+
+  test("Can write/read large number by jsoniter without loosing precision") {
+    val largeNum = "9999999999999999999999999999999911"
+    val jsValue = play.api.libs.json.JsNumber(BigDecimal(largeNum))
+    val bytes = PlayJsonJsoniter.serialize(jsValue)
+    new String(bytes) shouldEqual largeNum
+    PlayJsonJsoniter.deserialize(bytes) shouldEqual jsValue
+  }
+
+  test("Can parse max double string as play json") {
+    val json = s"""{ "max":$maxDoubleStr }"""
+    val jsValue0 = Json.parse(json.getBytes)
+    val jsValue1 = PlayJsonJsoniter.deserialize(json.getBytes)
+    jsValue0 shouldEqual jsValue1
   }
 }
