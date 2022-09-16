d48c61fda7aed22ac1b56775a3f6756e9e42cb26
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2022-09-16 10:55:31 +0200

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
 
-  val shapeless = "com.chuusai"         %% "shapeless" % "2.3.3"
+  val shapeless = "com.chuusai"         %% "shapeless" % "2.3.10"
   val nel       = "com.evolutiongaming" %% "nel"       % "1.3.4"
   val playJson  = "com.typesafe.play"   %% "play-json" % "2.10.0-RC6"
-  val scalaTest = "org.scalatest"       %% "scalatest" % "3.2.12"
+  val scalaTest = "org.scalatest"       %% "scalatest" % "3.2.13"
   val jsoniter  = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"  % "2.17.3"
   val jsonGenerator = "com.github.imrafaelmerino" %% "json-scala-values-generator" % "1.0.0"
-  val collectionCompact = "org.scala-lang.modules" %% "scala-collection-compat" % "2.7.0"
+  val collectionCompact = "org.scala-lang.modules" %% "scala-collection-compat" % "2.8.1"
 
   def excludeLog4j(moduleID: ModuleID): ModuleID = moduleID.excludeAll(
     ExclusionRule("log4j", "log4j"),
