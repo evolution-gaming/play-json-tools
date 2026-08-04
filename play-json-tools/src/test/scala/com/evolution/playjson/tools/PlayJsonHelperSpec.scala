be267dced7f0c624d5bad3845703b318313ad1d3
amikhailov <amikhailov@evolution.com>
2026-08-04 11:22:23 +0200

    Add contract tests for the Instant, LocalTime and FiniteDuration formats

--- play-json-tools/src/test/scala/com/evolution/playjson/tools/PlayJsonHelperSpec.scala
+++ play-json-tools/src/test/scala/com/evolution/playjson/tools/PlayJsonHelperSpec.scala
@@ -6,6 +6,9 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import play.api.libs.json._
 
+import java.time.{Instant, LocalTime}
+import scala.concurrent.duration._
+
 class PlayJsonHelperSpec extends AnyFunSuite with Matchers {
 
   test("nelFormat") {
@@ -50,6 +53,56 @@ class PlayJsonHelperSpec extends AnyFunSuite with Matchers {
     val json = Json.obj("nestedData" -> Json.obj("value" -> 123))
     format.writes(data) shouldEqual json
     format.reads(json) shouldEqual JsSuccess(data)
+  }
+
+  test("finiteDurationFormat round-trips a duration") {
+    val value = 1.minute
+    val json = Json.toJson(value)
+    json shouldEqual JsString("1 minute")
+    Json.fromJson[FiniteDuration](json) shouldEqual JsSuccess(value)
+  }
+
+  test("finiteDurationFormat reads a number as milliseconds") {
+    Json.fromJson[FiniteDuration](JsNumber(1500)) shouldEqual JsSuccess(1500.millis)
+  }
+
+  test("finiteDurationFormat reports a string it cannot parse") {
+    Json.fromJson[FiniteDuration](JsString("garbage")) shouldBe a[JsError]
+  }
+
+  test("finiteDurationFormat reports a duration that is not finite") {
+    Json.fromJson[FiniteDuration](JsString("Inf")) shouldBe a[JsError]
+  }
+
+  test("instantFormat round-trips, truncating to milliseconds") {
+    val value = Instant.parse("2026-08-03T10:15:30.123456Z")
+    val json = Json.toJson(value)
+    json shouldEqual JsString("2026-08-03T10:15:30.123Z")
+    Json.fromJson[Instant](json) shouldEqual JsSuccess(Instant.parse("2026-08-03T10:15:30.123Z"))
+  }
+
+  test("instantFormat reads an ISO-8601 instant") {
+    Json.fromJson[Instant](JsString("2026-08-03T10:15:30Z")) shouldEqual
+      JsSuccess(Instant.parse("2026-08-03T10:15:30Z"))
+  }
+
+  test("instantFormat reads a number as epoch milliseconds") {
+    Json.fromJson[Instant](JsNumber(1785492930123L)) shouldEqual JsSuccess(Instant.ofEpochMilli(1785492930123L))
+  }
+
+  test("instantFormat reports a string it cannot parse") {
+    Json.fromJson[Instant](JsString("garbage")) shouldBe a[JsError]
+  }
+
+  test("localTimeFormat round-trips") {
+    val value = LocalTime.of(10, 15, 30)
+    val json = Json.toJson(value)
+    json shouldEqual JsString("10:15:30")
+    Json.fromJson[LocalTime](json) shouldEqual JsSuccess(value)
+  }
+
+  test("localTimeFormat reports a string it cannot parse") {
+    Json.fromJson[LocalTime](JsString("garbage")) shouldBe a[JsError]
   }
 
   case object ConstObject
